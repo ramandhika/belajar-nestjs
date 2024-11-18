3ac461f7344abbfea36baaf82a94b71c021779e9
Ramandhika <ramandhika.ilham@gmail.com>
2024-11-18 11:16:58 +0700

    add database configuration and dotenv support for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot ({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
+const port = parseInt(process.env.APP_PORT) ?? 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.APP_PORT) ?? 3000);
+  await app.listen(port);
 
-  Logger.log(`Jalan Sudah dibuat 🚀 ` + `http://localhost:${process.env.APP_PORT}`, 'Runing Port');
+  Logger.log(`Jalan Sudah dibuat 🚀 ` + `http://localhost:${port}`, 'Runing Port');
 }
 bootstrap();
