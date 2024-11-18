254b6c92ca4ff176b9d65d038d664b39246a6b84
Ramandhika <ramandhika.ilham@gmail.com>
2024-11-18 10:16:47 +0700

    add dotenv package and configure environment variables for application port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(parseInt(process.env.APP_PORT) ?? 3000);
+
+  Logger.log(`Jalan Sudah dibuat 🚀 ` + `http://localhost:${process.env.APP_PORT}`, 'Runing Port');
 }
 bootstrap();
